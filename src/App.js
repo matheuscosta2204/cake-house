c263a27bd87d579dd04a1080696a3caf64e67857
Matheus Costa <matheus.costa@baraodemaua.br>
2019-03-14 17:39:18 -0300

    Config added

--- src/App.js
+++ src/App.js
@@ -1,40 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
 
-  scrollToMyRef = () => window.scrollTo({
-                          top: this.aRef.offsetTop,
+  scrollToMyRef = (ref) => window.scrollTo({
+                          top: ref,
                           left: 0,
                           behavior: 'smooth'
                         });
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <a
-            onClick={() => this.scrollToMyRef()}
-          >
-            Press me
-          </a>
-        </header>
-        <section ref={ (ref) => this.aRef = ref } style={{ height: '1000px' }}>
-          Ola Mundo
-        </section>
+      <div>
+       
       </div>
     );
   }
